8ee65d464bc54506fbc3a67629331c5bbd978877
Tejas Surse <205771957+tejas-surse-porfyro@users.noreply.github.com>
2025-05-07 19:40:02 +0530

    sdfa

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,20 @@
 const CACHE_NAME = 'todo-cache-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './style.css',
-  './script.js',
-  './todo.jpg',
-  './manifest.json'
+const URLS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/todo.jpg',
+  '/manifest.json'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then((res) => res || fetch(event.request))
   );
 });
